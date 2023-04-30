c626b2f6685f9d2aeb9d01b93a4d56652b18cd01
abualmagd <ismailabualmagd@google.com>
2023-04-30 16:39:47 +0300

    data api  services

--- src/services/appServices.js
+++ src/services/appServices.js
@@ -1,5 +1,5 @@
 
-import { mybase } from "./global";
+import { getToken, mybase } from "./global";
 
 
 export async function addNewApp(object) {
@@ -17,7 +17,7 @@ export async function getAppByName(appName) {
 
 
 export async function getHeighlyApps(setOff) {
-    return await mybase.rpc('', setOff);
+    return await mybase.rpc('get_rand_apps_order_by_rating', setOff);
 }
 
 export async function getNewApps() {
@@ -34,3 +34,49 @@ export async function deleteApp(id) {
 }
 
 
+export async function getUserSavedApps(userId) {
+    return await mybase.rpc('get_saved_apps', userId);
+}
+
+export async function bookmark(userId, appId) {
+
+    return await mybase.from('favorite').insert({ user_id: userId, app_id: appId });
+
+}
+
+export async function createReview(appId, content, userRating) {
+    const { id, avatar_url, name } = getLocalUser();
+    return await mybase.from('reviews').insert({
+        app_id: appId,
+        user_id: id,
+        content: content,
+        rating: userRating,
+        creator_avatar_url: avatar_url,
+        creator_name: name,
+    }).select("*");
+}
+
+export async function getAppReviews(appId) {
+    return await mybase.from('reviews').select('*').eq('app_id', appId).limit(10);
+
+}
+
+export async function createReplay(replayTo, reviewId, content) {
+    const { id, avatar_url, name } = getLocalUser();
+    return await mybase.from('reviews').insert({
+        replay_to: replayTo,
+        review_id: reviewId,
+        content: content,
+        user_id: id,
+        creator_avatar_url: avatar_url,
+        creator_name: name,
+    }).select("*");
+}
+
+
+
+export async function getReplaysforReview(reviewId) {
+
+    return await mybase.from('replays').select('*').eq("review_id", reviewId).limit(10);
+
+}
